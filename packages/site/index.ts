f86f0869947d1a770f303e7638367b424e16c392
OhmRee <13455401+ohmree@users.noreply.github.com>
2021-10-05 01:37:50 +0300

    Pogface

--- packages/site/index.ts
+++ packages/site/index.ts
@@ -5,9 +5,9 @@ const hash = document.location.hash.substr(1);
 js.className = '';
 if (hash) {
   js.innerHTML = 'Redirecting...STFU!';
-  const oldLocation = document.location.origin;
+  const oldLocation = `${document.location.origin}${document.location.pathname}`;
   document.location.replace(`stfu://${hash}`);
-  document.location.replace(`${oldLocation}/#token_retrieved`);
+  document.location.replace(`${oldLocation}#token_retrieved`);
 } else {
   js.innerHTML = 'Error: Access Token not found.';
 }
