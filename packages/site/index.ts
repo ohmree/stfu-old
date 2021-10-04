a02ac010df71dd5524c4b94b2ea41736bd71986c
OhmRee <13455401+ohmree@users.noreply.github.com>
2021-10-05 01:25:37 +0300

    Trying to eliminate the token from the hash after redirecting

--- packages/site/index.ts
+++ packages/site/index.ts
@@ -5,7 +5,9 @@ const hash = document.location.hash.substr(1);
 js.className = '';
 if (hash) {
   js.innerHTML = 'Redirecting...STFU!';
+  const oldLocation = document.location.origin;
   document.location.replace(`stfu://${hash}`);
+  document.location.replace(`${oldLocation}/#token_retrieved`);
 } else {
   js.innerHTML = 'Error: Access Token not found.';
 }
