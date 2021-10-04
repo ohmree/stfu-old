dae212e4ceae3c1b1821c3c8af6098512921c618
OhmRee <13455401+ohmree@users.noreply.github.com>
2021-10-04 17:45:02 +0300

    Added github pages workflow

--- packages/site/vite.config.ts
+++ packages/site/vite.config.ts
@@ -2,5 +2,6 @@ import {defineConfig} from 'vite';
 import htmlMinimize from '@sergeymakinen/vite-plugin-html-minimize';
 
 export default defineConfig({
-    plugins: [htmlMinimize()]
+  plugins: [htmlMinimize()],
+  base: '/stfu/'
 })
