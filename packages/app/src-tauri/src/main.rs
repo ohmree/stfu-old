d5b4cc02061094c588c93f89e405f24345f80720
OhmRee <13455401+ohmree@users.noreply.github.com>
2021-10-05 21:26:51 +0300

    Removed custom protocol junk for now

--- packages/app/src-tauri/src/main.rs
+++ packages/app/src-tauri/src/main.rs
@@ -3,7 +3,6 @@
     windows_subsystem = "windows"
 )]
 
-use tauri::{Manager, http::ResponseBuilder};
 use tauri_plugin_store::Store;
 
 load_dotenv::load_dotenv!();
@@ -11,15 +10,6 @@ load_dotenv::load_dotenv!();
 fn main() {
     tauri::Builder::default()
         .plugin(Store::default())
-        .register_uri_scheme_protocol("stfu", move |app, request| {
-            let uri = &request.head.uri;
-            let start = uri.find('#');
-            if let Some(start) = start {
-                let fragment = &uri[start..uri.len()-1];
-                app.emit_all("stfu://token", fragment)?;
-            }
-            ResponseBuilder::new().body(Vec::new())
-        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
