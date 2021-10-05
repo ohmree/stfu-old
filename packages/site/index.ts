2039227e0d29582706e297a0ae07054986cab541
OhmRee <13455401+ohmree@users.noreply.github.com>
2021-10-05 21:19:25 +0300

    Tauri apis should be injected, now we can emit events

--- packages/site/index.ts
+++ packages/site/index.ts
@@ -1,13 +1,20 @@
+import {event} from '@tauri-apps/api';
 import './app.css';
 
 const js = document.querySelector('#javascript')!;
-const hash = document.location.hash.substr(1);
-js.className = '';
-if (hash) {
-  js.innerHTML = 'Redirecting...STFU!';
-  const oldLocation = `${document.location.origin}${document.location.pathname}`;
-  document.location.replace(`stfu://${hash}`);
-  document.location.replace(`${oldLocation}#token_retrieved`);
+if (window.rpc) {
+    const hash = document.location.hash.substr(1);
+    if (hash) {
+        js.className = '';
+        document.location.replace(`${document.location.pathname}#token_retrieved`);
+        event.emit('stfu://token', hash);
+    } else if (hash === 'token_retrieved') {
+        js.innerHTML = 'You will be redirected in a moment, STFU!';
+    } else {
+        js.innerHTML = 'Error: Access Token not found.';
+    }
 } else {
-  js.innerHTML = 'Error: Access Token not found.';
+    document.location.replace(`${document.location.pathname}#no_tauri`);
+    js.className = '';
+    js.innerHTML = 'Error: This page only works in the STFU companion app.';
 }
